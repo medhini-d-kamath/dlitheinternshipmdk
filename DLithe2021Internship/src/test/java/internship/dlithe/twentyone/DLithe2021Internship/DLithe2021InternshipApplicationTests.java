124803d1180a6c4e7f54a6f9e72b2d06dee3d6a1
Annamalai1995 <s.annamalai1995@gmail.com>
2021-04-03 21:03:37 +0530

    First Boot Project

---
+++ DLithe2021Internship/src/test/java/internship/dlithe/twentyone/DLithe2021Internship/DLithe2021InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package internship.dlithe.twentyone.DLithe2021Internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DLithe2021InternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
