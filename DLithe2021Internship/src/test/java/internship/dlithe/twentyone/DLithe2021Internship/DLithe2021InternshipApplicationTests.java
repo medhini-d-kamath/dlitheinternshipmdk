4ef223238ba8c2ef79a2a27e82c8672c9f42be2f
Annamalai1995 <s.annamalai1995@gmail.com>
2021-04-06 21:14:48 +0530

    Completion of Module 1

--- DLithe2021Internship/src/test/java/internship/dlithe/twentyone/DLithe2021Internship/DLithe2021InternshipApplicationTests.java
+++ DLithe2021Internship/src/test/java/internship/dlithe/twentyone/DLithe2021Internship/DLithe2021InternshipApplicationTests.java
@@ -1,13 +1,35 @@
 package internship.dlithe.twentyone.DLithe2021Internship;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import internship.dlithe.twentyone.DLithe2021Internship.controllers.RepoAccount;
+import internship.dlithe.twentyone.DLithe2021Internship.controllers.ServiceAccount;
+import internship.dlithe.twentyone.DLithe2021Internship.model.Account;
+
 
 @SpringBootTest
-class DLithe2021InternshipApplicationTests {
-
+@RunWith(SpringRunner.class)
+class DLithe2021InternshipApplicationTests 
+{
+	@MockBean
+	RepoAccount repo;
+	
+	@Autowired
+	ServiceAccount service;
+	
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testInsertion()
+	{
+		Account acc=new Account(987656789L, "Mohamed", "ABCD111", "SAlem", "razzaksr@gmail.com", "Savings", "abcabc", 9876568987L, 1200.8);
+	}
 }
